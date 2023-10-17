768383d6b9a606800db7784db21e389bac50bfec
Joshua Germon <joshgermon@gmail.com>
2023-10-17 22:05:31 +1100

    Fix docker, fetch with RSC and remove next routes

--- frontend/app/sessions/page.tsx
+++ frontend/app/sessions/page.tsx
@@ -1,37 +1,26 @@
-"use client";
-
 import { columns } from "@/components/sessions/columns";
 import { SessionForm } from "@/components/sessions/create-form";
 import { DataTable } from "@/components/ui/data-table";
-import { SessionData } from "@/lib/session";
-import { useQuery } from "@tanstack/react-query";
 
-async function getSessions() {
-    const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_ENDPOINT}/v1/bookings`,
-    );
-    const data = await res.json();
-    return data;
-}
+export default async function Sessions() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/v1/bookings`,
+  );
+  const bookings = await res.json();
 
-export default function Sessions() {
-    const query = useQuery({ queryKey: ["sessions"], queryFn: getSessions });
-    console.log(query.data);
-    if (query.isLoading) return "Loading...";
-    if (query.error) return "An error has occurred: " + query.error.message;
-    return (
-        <div className="w-100">
-            <div className="mb-6">
-                <h2 className="text-2xl font-semibold mb-1">Your Sessions</h2>
-                <p className="text-gray-500">
-                    This is your home dashboard where you can manage everything.
-                </p>
-            </div>
-            <DataTable
-                columns={columns}
-                data={query.data}
-                headerItems={<SessionForm />}
-            />
-        </div>
-    );
+  return (
+    <div className="w-100">
+      <div className="mb-6">
+        <h2 className="text-2xl font-semibold mb-1">Your Sessions</h2>
+        <p className="text-gray-500">
+          This is your home dashboard where you can manage everything.
+        </p>
+      </div>
+      <DataTable
+        columns={columns}
+        data={bookings}
+        headerItems={<SessionForm />}
+      />
+    </div>
+  );
 }
