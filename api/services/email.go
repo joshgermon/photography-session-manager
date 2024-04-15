334dce5de291659f0d29d186725efb35bb4b1308
joshua <joshgermon@gmail.com>
2024-04-15 20:31:14 +1000

    Initial start on email templating

--- api/services/email.go
+++ api/services/email.go
@@ -1,9 +1,28 @@
 package email
 
-func sendTransactionalEmail(templateID string) {
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"os"
+)
+
+func generateHTMLTemplate(templateName string, templateData interface{}) (string, error) {
+  rawHTML, err := os.ReadFile("./templates/booking-confirmation.html")
+  if err != nil {
+    fmt.Printf("Failed to open template")
+    return "", err
+  }
+
+  var outputHTML bytes.Buffer
+
+  tmpl, err := template.New("bookingConfirmation").Parse(string(rawHTML));
+  err = tmpl.Execute(&outputHTML, templateData)
+
+  return outputHTML.String(), nil
 
 }
 
-func sendBookingConfirmationEmail(booking Booking) {
+// func sendTransactionalEmail(templateID string) error {
+// }
 
-}
